50f77d9ca3e7293345c70457b38f555eb919b54b
Venu Kethapally <venuk1418@gmail.com>
2026-02-05 17:06:42 +0530

    Update app.js

--- app.js
+++ app.js
@@ -2,17 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('🚀 Welcome to My DevOps Project 🚀
-
-This project is designed to showcase modern DevOps practices:
-- Automated CI/CD pipelines
-- Cloud-native deployments on AWS
-- Infrastructure as Code
-- Monitoring & logging for reliability
-
-The goal is simple: streamline operations, ensure compliance, and deliver applications faster with confidence.
-
-Let’s build, deploy, and innovate together!'));
+app.get('/', (req, res) => res.send('Hello World!'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
